2c89663cc2071ecf7a64bdd53c7a8256885dc239
dsonyy <dsonyyy@gmail.com>
2020-06-01 00:29:38 +0200

    Codeforces 1358C

---
+++ codeforces/1358C/1358C.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        long long x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		cout << (x2 - x1) * (y2 - y1) + 1 << "\n";
+    }
+}
