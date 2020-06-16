dd92e55256afd2bdbfd8e848aa23f4ff4e7f57ed
dsonyy <dsonyyy@gmail.com>
2020-06-16 20:35:50 +0200

    Codeforces

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Algorithms and competetive programming stuff
 
+- [Code Forces](http://codeforces.com/)
+
 Sources:
 - [main2](https://www.main2.edu.pl/c/kurs-podstaw-algorytmiki-druga-e/p/) (pol)
 - [eduinf](https://eduinf.waw.pl/inf/alg/001_search/) (pol)
 - [Introduction to Algorithms](https://mitpress.mit.edu/books/introduction-algorithms-third-edition)
-- [Code Forces](http://codeforces.com/)
+
 - and others
---
+++ codeforces/1367B-even-array/main.cpp
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef _MSC_VER
+#include <bits/stdc++.h>
+#else
+#include <__msvc_all_public_headers.hpp>
+#endif
+using namespace std;
+
+#define for0(i, n)      for (int i = 0; i < (int)(n); ++i)
+#define for1(i, n)      for (int i = 1; i <= (int)(n); ++i)
+#define forc(i, l, r)   for (int i = (int)(l); i <= (int)(r); ++i)
+#define forr0(i, n)	    for (int i = (int)(n) - 1; i >= 0; --i)
+#define forr1(i, n)     for (int i = (int)(n); i >= 1; --i)
+#define wh(t)           while (t--)
+	
+#define endl "\n"
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.precision(10);
+
+	//freopen("out.txt", "w", stdout);
+	//freopen("in.txt", "r", stdin);
+
+	int t;
+	cin >> t;
+	wh(t) {
+		int n;
+		cin >> n;
+		int a = 0, b = 0;
+		for0(i, n) {
+			int x;
+			cin >> x;
+			if (i % 2 != x % 2 && x % 2 == 0) a++;
+			else if (i % 2 != x % 2 && x % 2 == 1) b++;
+		}
+		if (a != b) cout << -1 << endl;
+		else cout << a << endl;
+	}
+}
